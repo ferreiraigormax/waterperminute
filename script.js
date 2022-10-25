6229c4768e160bfe5ea1e2fe7bb2a13630f86110
ferreiraigormax <igormaxferreira_stz@hotmail.com>
2022-10-25 13:09:25 -0300

    Incluido a função calcular minutos e calcular litros.

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 //Este é o código javascript do projeto, para este projeto eu vou precisar dos seguintes requisitos:
-//1 - Conseguir capturar o relógio do usuário, para pegar os minutos e segundos do mesmo.
+
+//1 - Criar um contador e alimentar o mesmo com a quantidade de minutos.
+let minuto = 0;
+let litros = 0;
+
+function sleep(ms){
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function contar_minuto(){
+  while (true){
+  minuto += 1
+  document.querySelector("#minuto").innerText = minuto
+  await sleep(1 * 1000);
+  }
+}
+contar_minuto();
+
 //2 - Buscar uma formula para calcular a quantidade de litros de água desperdiçados no Brasil por minutos/segundos.
+
+async function contar_litros(){
+    while (true){
+    document.querySelector("#litros").innerText = litros
+    await sleep(1 * 60000);
+    litros += 10.0619
+    }
+}
+contar_litros();
+
 //3 - Modificar em tempo real através de CSS a quantidade de água sendo desperdiçada.
 
-var minuto = 0
-var segundo = 0
 
-while (segundo < 100){
-    segundo += 1
-    document.querySelector('#segundo').innerHTML = segundo
-    if (segundo === 60){
-        minuto += 1
-        document.querySelector('#minuto').innerHTML = minuto
-    }
-    else{
-    }
 
-}
